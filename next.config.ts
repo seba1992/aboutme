d645a3a6e414ddcc0b26ae69ebc8b1bd06fe4860
seba dev (github) <seba242409@gmail.com>
2025-01-28 05:07:53 +0000

    Add assetPrefix config in nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProd ? '/aboutme' : '',
   basePath: isProd ? '/aboutme' : '',
   images: {
     unoptimized: true, // Disable default image optimization
