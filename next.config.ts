8feff4a993f1508a9176fbf60cad9e509e0dab63
seba dev (github) <seba242409@gmail.com>
2025-01-28 04:43:46 +0000

    Preparing for github pages... WIP

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? '/aboutme' : '',
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
   /* config options here */
 };
 
