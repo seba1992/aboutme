6310849d864546bc1c39c5f2b47d72c4b36ceac3
seba dev (github) <seba242409@gmail.com>
2025-02-04 17:08:08 +0000

    Add article component

---
+++ app/Article.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import Image, {StaticImageData} from 'next/image';
+
+type ArticleProps = {
+    date: string
+    title: string
+    description: React.ReactNode
+    image: StaticImageData
+};
+
+export default function Article ({ date,title, description, image }: ArticleProps) {
+    return (
+        <div className="rounded-lg border-3 p-4 bg-gray-100">
+            <div className="text-blue-500">{date}</div>
+            <div className="font-bold text-xl">{title}</div>
+            <div>
+                {description}
+                <Image
+                    className="dark:invert mx-auto rounded-lg m-5"
+                    src={image.src}
+                    alt={title}
+                    width={300}
+                    height={200}
+                    priority
+                />
+            </div>
+        </div>
+    );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import imgEngranajes from "../public/images/engranajes_1.png"
+
+import Article from "./Article"
 
 export default function Home() {
   return (
@@ -7,21 +8,15 @@ export default function Home() {
       <div className="max-w-md text-left w-full">
         <h1 className="text-white text-xl mt-5 mb-5"><b>{"<"}IndieSev{"/>"}</b>  About me</h1>
       </div>
-      <main className="items-center sm:items-start max-w-md">   
-       <div className="rounded-lg border-3 p-4 bg-gray-100">
-        <div className="text-blue-500">26 de enero de 2025</div>
-        <div className="font-bold text-xl">aijs.io + p5.js</div>
-        <div>En ratos de ocio, suelo utilizar p5.js ya sea desde su propio editor o desde openProcessing, sin embargo, mientras realizaba un framework para realizar animaciones de forma práctica e intuitiva en python, en el que me terminé topando con una sintaxis similar a la usada por p5play.js, y volver a p5play, me términe topando con aijs, que no es otra cosa más que un editor cuasi IDE con un chat ai integrado. Les dejo una mini muestra de una animación que realicé a modo de prueba... <b>Continuará</b></div>
-        <Image
-          className="dark:invert mx-auto rounded-lg m-5"
-          src={imgEngranajes}
-          alt="Engranajes captura aijs.io v2"
-          width={300}
-          height={200}
-          priority
+      <main className="items-center sm:items-start max-w-md">    
+       <Article 
+          date = "26 de enero de 2025" 
+          title="aijs.io + p5.js" 
+          description = {
+            <p>En ratos de ocio, <b>suelo utilizar</b> p5.js ya sea desde su propio editor o desde openProcessing, sin embargo, mientras realizaba un framework para realizar animaciones de forma práctica e intuitiva en python, en el que me terminé topando con una sintaxis similar a la usada por p5play.js, y volver a p5play, me términe topando con aijs, que no es otra cosa más que un editor cuasi IDE con un chat ai integrado. Les dejo una mini muestra de una animación que realicé a modo de prueba... <b>Continuará</b></p>}
+          image = {imgEngranajes}
         />
-       </div>  
-      </main>
+        </main>
     </div>
   );
 }
